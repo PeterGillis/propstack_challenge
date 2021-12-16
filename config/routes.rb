fd70ddf9394829182b0cd4f89dc0729bb80d518e
Peter Gillis <petergillis@hotmail.com>
2021-12-16 20:55:17 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :spaceships
+  resources :missions
+  resources :crew_members
+  # resources :model, only: [:index]
 end

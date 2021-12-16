1b408d631a0ac94c20df44d4276e16dc815ece86
Peter Gillis <petergillis@hotmail.com>
2021-12-16 20:54:37 +0100

    Add spec and factory setup

---
+++ spec/factories/spaceship.rb
@@ -0,0 +1,4 @@
+FactoryBot.define do
+  factory :spaceship do
+  end
+end
---
+++ spec/requests/spaceships_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Spaceships', type: :request do
+  describe 'GET /index' do
+    it 'returns all Spaceships' do
+      FactoryBot.create(:spaceship, name: 'Jupiter 2', crew: 6)
+      FactoryBot.create(:spaceship, name: 'Apollo 11', crew: 4)
+
+      get '/spaceships'
+
+      expect(response).to have_http_status(:success)
+      # byebug
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
+end

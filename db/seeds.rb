5be3c22555853ea04f08a8fe55ca2ebd4cde7ef5
Peter Gillis <petergillis@hotmail.com>
2022-02-15 11:26:19 +0100

    Move puts statemnet to the end

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@ FactoryBot.create(:spaceship, name: 'Defiant', crew: 70)
 FactoryBot.create(:spaceship, name: 'Jupiter 2', crew: 6)
 FactoryBot.create(:spaceship, name: 'Apollo 11', crew: 4)
 
-puts '✔ database seeded ✔'
 FactoryBot.create(:crew_member, name: 'Peter', age: 37, rank: 'captain', assignment: 'ground control', spaceship_id: 1)
 FactoryBot.create(:crew_member, name: 'Karen', age: 32, rank: 'Admiral', assignment: 'Boss', spaceship_id: 2)
 FactoryBot.create(:crew_member, name: 'Karen', age: 31, rank: 'Liutenant', assignment: 'Deputy', spaceship_id: 3)
@@ -15,3 +14,4 @@ FactoryBot.create(:crew_member, name: 'Karen', age: 31, rank: 'Liutenant', assig
 FactoryBot.create(:mission, name: 'Ground Control', description: 'To Major Tom', length: 3, start_date: '2021-12-16', crew_member_id: 1)
 FactoryBot.create(:mission, name: 'Take your protein pills', description: 'Put your helmet on', length: 2, start_date: '2021-12-15', crew_member_id: 2)
 FactoryBot.create(:mission, name: 'Commencing countdown', description: 'Engines on', length: 4, start_date: '2021-12-15', crew_member_id: 3)
+puts '✔ database seeded ✔'
